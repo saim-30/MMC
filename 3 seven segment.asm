37387dd6561b05629d7d1fdf73b59cbb2d0a00d6
saim-30 <saaimshaikh30@gmail.com>
2025-11-11 08:28:31 +0530

    Refactor 7-segment display code for input handling

--- 3 seven segment.asm
+++ 3 seven segment.asm
@@ -1,18 +1,16 @@
 ORG 0000H
-
-MOV TMOD, #10H       ; Timer1 in Mode 1 (16-bit timer)
+MOV P1, #0FFH      ; Set Port 1 as input (all bits high)
+MOV P2, #00H       ; Clear Port 2 (output port)
+MOV DPTR, #0300H   ; Load lookup table address into DPTR
 
 AGAIN: 
-    MOV TL1, #0FFH    ; Load low byte of Timer1
-    MOV TH1, #0EDH    ; Load high byte of Timer1
-    CPL P1.5          ; Toggle pin P1.5
-    SETB TR1          ; Start Timer1
+    MOV A, P1       ; Read input from Port 1
+    ANL A, #0FH     ; Mask lower 4 bits (take only lower nibble)
+    MOVC A, @A+DPTR ; Get corresponding code from lookup table
+    MOV P2, A       ; Send the output to Port 2
+    SJMP AGAIN      ; Repeat forever
 
-BACK: 
-    JNB TF1, BACK     ; Wait until Timer1 overflows (TF1 = 1)
-
-    CLR TR1           ; Stop Timer1
-    CLR TF1           ; Clear overflow flag
-    SJMP AGAIN        ; Repeat forever
+ORG 0300H           ; Lookup table for 7-segment display (0–9)
+DB 3FH, 06H, 5BH, 4FH, 66H, 6DH, 7DH, 07H, 7FH, 6FH
 
 END
